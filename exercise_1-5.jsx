8dd5301f6af5298fff3ae4c6ea20a3917816eaca
CatmanThree <153368687+CatmanThree@users.noreply.github.com>
2026-01-04 16:45:07 +0000

    Create exercise_1-5.jsx

---
+++ exercise_1-5.jsx
@@ -0,0 +1,64 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </>
+  );
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.exercises
+        .map((val) => val.exercises)
+        .reduce((acc, current) => acc + current)}
+    </p>
+  );
+}
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total exercises={course.parts} />
+    </div>
+  );
+};
+
+export default App;
